df3d151a7c9f51d877eb07d7d11f293015737287
ksughosh <k.sughosh@gmail.com>
2018-03-26 21:48:28 +0200

    updated the samples for delegate onClick

--- sample/src/main/java/com/github/ksughosh/compositionadapter/MainActivity.kt
+++ sample/src/main/java/com/github/ksughosh/compositionadapter/MainActivity.kt
@@ -3,22 +3,29 @@ package com.github.ksughosh.compositionadapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import com.github.ksughosh.adapter.ViewDelegate
 import com.github.ksughosh.adapter.DataAdapter
 import com.github.ksughosh.adapter.ListItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var delegateOne: ViewDelegate<DataOne,DataOneView>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val delegateOne = ViewDelegate({ DataOneView(it?.context) }, { (it as? DataOne)?.isShown == false })
+        delegateOne = ViewDelegate({ DataOneView(it?.context) }, { (it as? DataOne)?.isShown == false })
         val delegateTwo = ViewDelegate({ DataTwoView(it?.context) }, { it is DataTwo })
         val delegateThree = ViewDelegate({ DataOneView2(it?.context) }, { (it as? DataOne)?.isShown == true})
 
-        listView?.adapter = DataAdapter(generateDummyList()).addDelegate(delegateOne, delegateTwo, delegateThree)
+        // example of on click for delegate
+        delegateOne?.setOnDelegateClick { data, position ->
+            Log.d("DELEGATE_CLICK", "OnClick of delegate with ${data.model?.isShown}, at position $position")
+        }
+
+        listView?.adapter = DataAdapter(generateDummyList()).addDelegate(delegateOne!!, delegateTwo, delegateThree)
         listView?.layoutManager = LinearLayoutManager(this)
     }
 
@@ -33,5 +40,12 @@ class MainActivity : AppCompatActivity() {
         list.add(DataOne(false))
         list.add(DataTwo(4, "name"))
         return list
+    }
+
+    override fun onDestroy() {
+        // clear references in the delegate to
+        // avoid potential leaks
+        delegateOne?.onDestroy()
+        super.onDestroy()
     }
 }
